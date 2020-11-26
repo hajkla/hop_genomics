d147a49361f68a725117bd069f0d22cd6b8ad7c4
hajkla <74966349+hajkla@users.noreply.github.com>
2020-11-26 12:59:42 +0000

    Add files via upload

--- pre_SNP_calling.sh
+++ pre_SNP_calling.sh
@@ -1,82 +1,82 @@
-#!/usr/bin/env bash
-#SBATCH -J pre_snp_calling
-#SBATCH --partition=long
-#SBATCH --mem-per-cpu=8G
-#SBATCH --cpus-per-task=8
 
 
-##############################################
-# Prep mappings from Bowtie2 for SNP calling
-### Remove multimapping reads, discordant reads. PCR and optical duplicates, and
-### add read group and sample name to each mapped read (preferably, the shortest ID possible)
-#INPUT:
-# 1st argument: input SAM file with your mappings
-# 2nd argument: sample name (prefix) to be used to identify it in the future
-#OUTPUT:
-# Indexed BAM file with suffix "nodup_rg" to be fed into SNP calling with GATK.
-#############################################
-input_sam=$1
-prefix=$2
-OutDir=$3
-filename=$(basename "$input_sam")
-name="${filename%.*}"
 
+-----------------------------------------------------------------Alignment---------------------------------------------------------------------
+# Alignemt of S1 S3 reads vs Cascade genome
 
-WorkDir=/projects/hop/${SLURM_JOB_USER}_${SLURM_JOBID}
-mkdir -p $WorkDir
-### Prep
+ Alignment of reads from a single run using Bowtie:
 
-cp $input_sam $WorkDir
-cd $WorkDir
+for Hop_genome in S1 S3; do
+  Reference=$(ls ../../projects/hop/raw_data/cascasde/cascadePrimary.fasta)
+  for StrainPath in $(ls -d ../../projects/hop/raw_data/trim/"$Hop_genome"); do
+    Strain=$(echo $StrainPath | rev | cut -f1 -d '/' | rev)
+    Organism=$(echo $StrainPath | rev | cut -f2 -d '/' | rev)
+    F_Read=$(ls $StrainPath/F/*_trim.fq.gz)
+    R_Read=$(ls $StrainPath/R/*_trim.fq.gz)
+    echo "$Organism - $Strain"
+    echo $F_Read
+    echo $R_Read
+    OutDir=$StrainPath/alignment
+    mkdir -p $OutDir
+    ProgDir=/home/hajduk/gitrepos/scripts
+    sbatch $ProgDir/bowtie.sh $Reference $F_Read $R_Read $OutDir
+  done
+done
 
-### Get rid of multimapping reads by filtering out on the XS:i: tag
-grep -v "XS:i" $filename >temp && mv temp $filename
-samtools view -bS -o $name.bam $filename
-samtools sort $name.bam -o $name\_nomulti\_sorted.bam
-#For samtools older versions (0.1.18) use: 
-#samtools sort $name.bam $name\_nomulti\_sorted
-samtools index $name\_nomulti\_sorted.bam
+#the line below converts bam files to sam files 
+for Strain in S1 S3; do 
+ for file in /projects/hop/raw_data/trim/alignment/cascadePrimary.fasta_aligned_sorted.bam
+  do
+     echo $file 
+     samtools view -h $file > ${file/.bam/.sam}
+ done
+done
 
-### Keep only reads with "paired reads" and "properly paired reads" flags.
-samtools view -b -h -f 3 -o $name\_nomulti\_proper.bam $name\_nomulti\_sorted.bam
-### Sort for downstream analyses
-samtools sort $name\_nomulti\_proper.bam -o $name\_nomulti\_proper\_sorted.bam
-#For samtools older versions (0.1.18) use: 
-#samtools sort $name\_proper.bam $name\_proper\_sorted
-samtools index $name\_nomulti\_proper\_sorted.bam
+-------------------------------------------------------------Renaming_files------------------------------------------------------------------
+## Rename input mapping files in each folder by prefixing with the strain ID
 
-### Remove PCR and optical duplicates
-picard=/home/connellj/miniconda2/share/picard-2.18.29-0/picard.jar
-java -jar $picard MarkDuplicates \
-	I=$name\_nomulti\_proper\_sorted.bam \
-	O=$name\_nomulti\_proper\_sorted\_nodup.bam \
-	METRICS_FILE=$name\_nomulti\_proper\_sorted\_nodup.txt \
-	REMOVE_DUPLICATES=TRUE \
-	ASSUME_SORTED=TRUE \
-	MAX_RECORDS_IN_RAM=500000000 \
-	MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=1000 \
-	VALIDATION_STRINGENCY=LENIENT 
+```bash
+for Strain in S3 ; do
+  for filename in /projects/hop/raw_data/trim/$Strain/alignment; do
+   echo $Strain
+     mv "$filename/cascadePrimary.fasta_aligned.sam" "$filename/"$Strain"_unmasked.fa_aligned.sam"
+     mv "$filename/cascadePrimary.fasta_aligned.bam" "$filename/"$Strain"_unmasked.fa_aligned.bam"
+     mv "$filename/cascadePrimary.fasta_aligned_sorted.bam" "$filename/"$Strain"_unmasked.fa_aligned_sorted.bam"
+     mv "$filename/cascadePrimary.fasta.indexed.1.bt2" "$filename/"$Strain"_contigs_unmasked.fa.indexed.1.bt2"
+     mv "$filename/cascadePrimary.fasta.indexed.2.bt2" "$filename/"$Strain"_contigs_unmasked.fa.indexed.2.bt2"
+     mv "$filename/cascadePrimary.fasta.indexed.3.bt2" "$filename/"$Strain"_contigs_unmasked.fa.indexed.3.bt2"
+     mv "$filename/cascadePrimary.fasta.indexed.4.bt2" "$filename/"$Strain"_contigs_unmasked.fa.indexed.4.bt2"
+     mv "$filename/cascadePrimary.fasta.indexed.rev.1.bt2" "$filename/"$Strain"_contigs_unmasked.fa.indexed.rev.1.bt2"
+     mv "$filename/cascadePrimary.fasta.indexed.rev.2.bt2" "$filename/"$Strain"_contigs_unmasked.fa.indexed.rev.2.bt2"
+     mv "$filename/cascadePrimary.fasta_RPK.txt" "$filename/"$Strain"_contigs_unmasked.fa_RPK.txt"
+     mv "$filename/cascadePrimary.fasta_aligned_sorted.bam.index" "$filename/"$Strain"_unmasked.fa_aligned_sorted.bam.index"
+  done 
+done 
+```
 
+--------------------------------------------------------------RemoveMultimapReadss----------------------------------------------------------
 
-### Add group and sample name (prefix)
-java -jar $picard AddOrReplaceReadGroups \
-	I=$name\_nomulti\_proper\_sorted\_nodup.bam \
-	O=$name\_nomulti\_proper\_sorted\_nodup_rg.bam \
-	SORT_ORDER=coordinate \
-	CREATE_INDEX=true \
-	RGID=$prefix \
-	RGSM=$prefix \
-	RGPL=Illumina \
-	RGLB=library \
-	RGPU=barcode \
-	VALIDATION_STRINGENCY=LENIENT 
+## Remove multimapping reads, discordant reads. PCR and optical duplicates, and add read group and sample name to each mapped read (preferably, the shortest ID possible)
 
+Convention used:
+qsub $ProgDir/sub_pre_snp_calling.sh <INPUT SAM FILE> <SAMPLE_ID>
 
-samtools index $name\_nomulti\_proper\_sorted\_nodup_rg.bam
+```bash
+  for Strain in S1 S3; do # Replace with the strain name
+    for input in ../../projects/hop/raw_data/trim/$Strain/alignment/"$Strain"_unmasked.fa_aligned.sam; do
+  #   Jobs=$(squeue -u ${USER} --noheader --array | wc -l)
+  #      while [ $Jobs -gt 1 ]; do
+  #          sleep 25m
+  #          printf "."
+  #          Jobs=$(squeue -u ${USER} --noheader --array | wc -l)
+  #      done
+  #  printf "\n"
+    OutDir=/projects/hop/raw_data/trim/$Strain/alignment/nomulti
+    mkdir -p $OutDir  
+    ProgDir=/home/hajduk/gitrepos/scripts/SNP_calling
+    sbatch $ProgDir/pre_SNP_calling.sh $input $Strain $OutDir # This will add read group and sample name to each mapped read. Preferably, use the shortest ID possible.
+    done
+  done
+ ```  
 
-### Cleanup
-mv $WorkDir/$name\_nomulti\_proper\_sorted\_nodup.txt $OutDir
-mv $WorkDir/$name\_nomulti\_proper\_sorted\_nodup_rg.bam $OutDir
-mv $WorkDir/$name\_nomulti\_proper\_sorted\_nodup_rg.bam.bai $OutDir
-#rm -rf $WorkDir
-
+ 
